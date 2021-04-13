bf0163c28dacf55a3970101953efec4d561c0bd6
Matheus Lopes <matlopes1999@gmail.com>
2021-04-13 08:28:34 -0300

    Adiciona Lista no sidebar

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+import { List, Avatar, Typography } from 'antd';
 
 import {AppRoute} from '../routes'
 import ROUTES_CONSTANTS from '../routes.constants'
@@ -9,10 +10,27 @@ import EmptyPage from '../empty/Empty';
 
 import { ChatWrapper, ChatSider } from './styled.chat';
 
+const {Title} = Typography;
+
 function ChatPage() {
     return (
         <ChatWrapper style={{height: '100%'}}>
-            <ChatSider style={{ background: '#fff' }} width={300}>Sider</ChatSider>
+            <ChatSider style={{ background: '#fff' }} width={300}>
+                <List
+                    style={{ padding: '16px 24px' }}
+                    header={<Title level={3}>Contatos</Title>}
+                >
+                    <List.Item onClick={() => console.log('click')}>
+                        <List.Item.Meta
+                            avatar={
+                                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                            }
+                            title="Teste"
+                            description="Meta description"
+                        />
+                    </List.Item>
+                </List>
+            </ChatSider>
 
             <Switch>
                 <AppRoute path={ROUTES_CONSTANTS.ROOM} component={RoomPage} />
