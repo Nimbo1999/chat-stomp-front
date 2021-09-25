cd5082bc6247e423e77c6e5c9a87fb376ba71efa
Matheus Lopes <matlopes1999@gmail.com>
2021-09-25 13:49:03 -0300

    Realiza o submit do formulário quando estiver com o foco no textarea e apertar enter + ctrl

--- src/components/commentary/CommentaryInput.js
+++ src/components/commentary/CommentaryInput.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Input, Button, Alert } from 'antd';
+import React, { useRef } from 'react';
+import { Input, Alert, Button } from 'antd';
 
 import { CommentaryWrapper, Container } from './styled.commentary';
 import { useRoomContext } from '../../context/RoomContext';
@@ -7,30 +7,35 @@ import { useRoomContext } from '../../context/RoomContext';
 const { TextArea } = Input;
 
 function CommentaryInput() {
-
     const { onSubmitMessage, handleOnChangeMessage, textMessage, error } = useRoomContext();
+
+    const submitButton = useRef(null);
+
+    const onPressEnterInTextArea = event => {
+        event.stopPropagation();
+
+        if (event.ctrlKey && submitButton.current) {
+            return submitButton.current.click();
+        }
+    };
 
     return (
         <Container>
+            {error && <Alert message={error} type="error" />}
 
-            {error && (
-                <Alert message={ error } type="error" />
-            )}
-
-            <CommentaryWrapper onSubmit={ onSubmitMessage }>
-
+            <CommentaryWrapper onSubmit={onSubmitMessage}>
                 <TextArea
                     placeholder="Mensagem..."
                     size="large"
-                    onChange={ handleOnChangeMessage }
-                    value={ textMessage }
+                    onChange={handleOnChangeMessage}
+                    value={textMessage}
                     autoSize
+                    onPressEnter={onPressEnterInTextArea}
                 />
 
-                <Button type="primary" htmlType="submit" size="large">
+                <Button type="primary" htmlType="submit" size="large" ref={submitButton}>
                     Enviar
                 </Button>
-
             </CommentaryWrapper>
         </Container>
     );
--- src/components/commentary/styled.commentary.js
+++ src/components/commentary/styled.commentary.js
@@ -1,18 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+const Container = styled.div`
     display: flex;
     flex-direction: column;
     gap: ${({ theme }) => theme.spacing(1)};
 `;
 
-export const CommentaryWrapper = styled.form`
+const CommentaryWrapper = styled.form`
     flex: 1;
     display: flex;
     justify-content: center;
-    align-items: flex-start;
+    align-items: center;
 
     textarea {
-        margin: 0px ${({theme}) => theme.spacing(2)} 0px 0px;
+        margin: 0px ${({ theme }) => theme.spacing(2)} 0px 0px;
+        max-height: 140px !important;
     }
 `;
+
+export { Container, CommentaryWrapper };
