4dd779328ae8d3d42fab162bae6dfbad0a754b50
Matheus Lopes <matlopes1999@gmail.com>
2021-10-10 11:52:25 -0300

    Finaliza tratamento de erro nas requisições http

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -3,6 +3,7 @@ import { API_CONSTANTS } from '../constants/api.constants';
 import InternalServerError from '../exceptions/InternalServerError';
 import BadRequestError from '../exceptions/BadRequestError';
 import handleHttpRequestErrors from '../exceptions/HandleHttpRequestErrors';
+import HTTP_RESPONSE_CODE from '../constants/HttpResponseCode';
 
 class HttpService {
     constructor() {
@@ -39,18 +40,30 @@ class HttpService {
     }
 
     checkResponse(response) {
-        if (response.status === 400)
-            throw new BadRequestError(
-                'Não foi possível acessar esse recurso, tente novamente mais tarde. Bad Request'
-            );
-        if (response.status === 500)
-            throw new InternalServerError(
-                'Não foi possível acessar esse recurso, tente novamente mais tarde. Internal Server Error'
-            );
+        HttpService.validateResponseStatus(response);
 
         if (response.headers.get('Content-Type') === 'application/json') return response.json();
 
         return response.text();
+    }
+
+    static validateResponseStatus(response) {
+        if (!response) return;
+
+        switch (response.status) {
+            case HTTP_RESPONSE_CODE.BAD_REQUEST:
+                throw new BadRequestError(
+                    'Não foi possível acessar esse recurso, tente novamente mais tarde. Bad Request'
+                );
+
+            case HTTP_RESPONSE_CODE.INTERNAL_SERVER_ERROR:
+                throw new InternalServerError(
+                    'Não foi possível acessar esse recurso, tente novamente mais tarde. Internal Server Error'
+                );
+
+            default:
+                return;
+        }
     }
 
     postHeaders() {
